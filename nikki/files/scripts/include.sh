027176f23d19b6e66db221eba564566854c2e460
ifjjyj <ifjjyj@gmail.com>
2026-05-27 15:19:17 +0800

    Update include.sh

--- nikki/files/scripts/include.sh
+++ nikki/files/scripts/include.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 # paths
 HOME_DIR="/etc/nikki"
 PROFILES_DIR="$HOME_DIR/profiles"
@@ -10,35 +9,29 @@ RUN_PROFILE_PATH="$RUN_DIR/config.yaml"
 PROVIDERS_DIR="$RUN_DIR/providers"
 RULE_PROVIDERS_DIR="$PROVIDERS_DIR/rule"
 PROXY_PROVIDERS_DIR="$PROVIDERS_DIR/proxy"
-
 # log
 LOG_DIR="/var/log/nikki"
 APP_LOG_PATH="$LOG_DIR/app.log"
 CORE_LOG_PATH="$LOG_DIR/core.log"
-
 # temp
 TEMP_DIR="/var/run/nikki"
 PID_FILE_PATH="$TEMP_DIR/nikki.pid"
 STARTED_FLAG_PATH="$TEMP_DIR/started.flag"
 BRIDGE_NF_CALL_IPTABLES_FLAG_PATH="$TEMP_DIR/bridge_nf_call_iptables.flag"
 BRIDGE_NF_CALL_IP6TABLES_FLAG_PATH="$TEMP_DIR/bridge_nf_call_ip6tables.flag"
-
 # ucode
 UCODE_DIR="$HOME_DIR/ucode"
 INCLUDE_UC="$UCODE_DIR/include.uc"
 MIXIN_UC="$UCODE_DIR/mixin.uc"
 HIJACK_UT="$UCODE_DIR/hijack.ut"
-
 # scripts
 SH_DIR="$HOME_DIR/scripts"
 INCLUDE_SH="$SH_DIR/include.sh"
 FIREWALL_INCLUDE_SH="$SH_DIR/firewall_include.sh"
-
 # nftables
 NFT_DIR="$HOME_DIR/nftables"
 GEOIP_CN_NFT="$NFT_DIR/geoip_cn.nft"
 GEOIP6_CN_NFT="$NFT_DIR/geoip6_cn.nft"
-
 # functions
 format_filesize() {
 	local b; b=1
@@ -64,7 +57,6 @@ format_filesize() {
 		fi
 	fi
 }
-
 prepare_files() {
 	if [ ! -d "$LOG_DIR" ]; then
 		mkdir -p "$LOG_DIR"
@@ -79,7 +71,6 @@ prepare_files() {
 		mkdir -p "$TEMP_DIR"
 	fi
 }
-
 log() {
 	echo "[$(date "+%Y-%m-%d %H:%M:%S")] [$1] $2" >> "$APP_LOG_PATH"
 }
